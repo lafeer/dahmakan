d2b264634665b1ee896d96a4366513f8b7a7a818
lafeer <lafeer92@gmail.com>
2019-07-20 19:44:28 +0800

    feat: Load request on componentDidMount and set response data to state
    # use try/catch block to catch errors
    # use async/await

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orders: null,
+    }
+  }
+
+  componentDidMount = async () => {
+    try {
+      const data = await axios.get('http://staging-api.dahmakan.com/test/orders').then(res => res.data)
+      const { orders } = data;
+      console.log(data)
+      this.setState({
+        orders,
+      }, () => console.log(this.state.orders));
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
 
 export default App;
